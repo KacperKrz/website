8316f2a194555f0884e7a34ba4fc1d11b977bb68
KacperKrz <71610552+KacperKrz@users.noreply.github.com>
2022-04-29 13:11:21 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,388 @@
+
+function hintButton1() {
+  alert("They are part of the Voorhees family");
+}
+
+function hintButton2() {
+  alert("It is a 4 digit number");
+}
+
+function hintButton3() {
+  alert("His movies are well known for their body horror special effects");
+}
+
+function hintButton4() {
+  alert("He also appeared in 1989's The Phantom of the Opera");
+}
+
+function hintButton5() {
+  alert("He is played by Devon Sawa");
+}
+
+function hintButton6() {
+  alert("It is famously attached to his glove");
+}
+
+function hintButton7() {
+  alert("The hopsital featured a prison-style outside yard");
+}
+
+function hintButton8() {
+  alert("The Texas Chain Saw Massacare is regarded as an influelentual 70s movie");
+}
+
+function a1Q1() {
+	document.getElementById('q1A1').className = 'item2clicked';
+	document.getElementById('q1A2').className = 'item3';
+	document.getElementById('q1A3').className = 'item4';
+	document.getElementById('q1A4').className = 'item5';
+	window.localStorage.setItem("q1", "1");
+}
+
+function a2Q1() {
+	document.getElementById('q1A1').className = 'item2';
+	document.getElementById('q1A2').className = 'item3clicked';
+	document.getElementById('q1A3').className = 'item4';
+	document.getElementById('q1A4').className = 'item5';
+	window.localStorage.setItem("q1", "2");
+}
+
+function a3Q1() {
+	document.getElementById('q1A1').className = 'item2';
+	document.getElementById('q1A2').className = 'item3';
+	document.getElementById('q1A3').className = 'item4clicked';
+	document.getElementById('q1A4').className = 'item5';
+	window.localStorage.setItem("q1", "3");
+}
+
+function a4Q1() {
+	document.getElementById('q1A1').className = 'item2';
+	document.getElementById('q1A2').className = 'item3';
+	document.getElementById('q1A3').className = 'item4';
+	document.getElementById('q1A4').className = 'item5clicked';
+	window.localStorage.setItem("q1", "4");
+}
+
+function a1Q2() {
+	document.getElementById('q2A1').className = 'item2clicked';
+	document.getElementById('q2A2').className = 'item3';
+	document.getElementById('q2A3').className = 'item4';
+	document.getElementById('q2A4').className = 'item5';
+	window.localStorage.setItem("q2", "1");
+}
+
+function a2Q2() {
+	document.getElementById('q2A1').className = 'item2';
+	document.getElementById('q2A2').className = 'item3clicked';
+	document.getElementById('q2A3').className = 'item4';
+	document.getElementById('q2A4').className = 'item5';
+	window.localStorage.setItem("q2", "2");
+}
+
+function a3Q2() {
+	document.getElementById('q2A1').className = 'item2';
+	document.getElementById('q2A2').className = 'item3';
+	document.getElementById('q2A3').className = 'item4clicked';
+	document.getElementById('q2A4').className = 'item5';
+	window.localStorage.setItem("q2", "3");
+}
+
+function a4Q2() {
+	document.getElementById('q2A1').className = 'item2';
+	document.getElementById('q2A2').className = 'item3';
+	document.getElementById('q2A3').className = 'item4';
+	document.getElementById('q2A4').className = 'item5clicked';
+	window.localStorage.setItem("q2", "4");
+}
+
+function a1Q3() {
+	document.getElementById('q3A1').className = 'item2clicked';
+	document.getElementById('q3A2').className = 'item3';
+	document.getElementById('q3A3').className = 'item4';
+	document.getElementById('q3A4').className = 'item5';
+	window.localStorage.setItem("q3", "1");
+}
+
+function a2Q3() {
+	document.getElementById('q3A1').className = 'item2';
+	document.getElementById('q3A2').className = 'item3clicked';
+	document.getElementById('q3A3').className = 'item4';
+	document.getElementById('q3A4').className = 'item5';
+	window.localStorage.setItem("q3", "2");
+}
+
+function a3Q3() {
+	document.getElementById('q3A1').className = 'item2';
+	document.getElementById('q3A2').className = 'item3';
+	document.getElementById('q3A3').className = 'item4clicked';
+	document.getElementById('q3A4').className = 'item5';
+	window.localStorage.setItem("q3", "3");
+}
+
+function a4Q3() {
+	document.getElementById('q3A1').className = 'item2';
+	document.getElementById('q3A2').className = 'item3';
+	document.getElementById('q3A3').className = 'item4';
+	document.getElementById('q3A4').className = 'item5clicked';
+	window.localStorage.setItem("q3", "4");
+}
+
+function a1Q4() {
+	document.getElementById('q4A1').className = 'item2clicked';
+	document.getElementById('q4A2').className = 'item3';
+	document.getElementById('q4A3').className = 'item4';
+	document.getElementById('q4A4').className = 'item5';
+	window.localStorage.setItem("q4", "1");
+}
+
+function a2Q4() {
+	document.getElementById('q4A1').className = 'item2';
+	document.getElementById('q4A2').className = 'item3clicked';
+	document.getElementById('q4A3').className = 'item4';
+	document.getElementById('q4A4').className = 'item5';
+	window.localStorage.setItem("q4", "2");
+}
+
+function a3Q4() {
+	document.getElementById('q4A1').className = 'item2';
+	document.getElementById('q4A2').className = 'item3';
+	document.getElementById('q4A3').className = 'item4clicked';
+	document.getElementById('q4A4').className = 'item5';
+	window.localStorage.setItem("q4", "3");
+}
+
+function a4Q4() {
+	document.getElementById('q4A1').className = 'item2';
+	document.getElementById('q4A2').className = 'item3';
+	document.getElementById('q4A3').className = 'item4';
+	document.getElementById('q4A4').className = 'item5clicked';
+	window.localStorage.setItem("q4", "4");
+}
+
+function a1Q5() {
+	document.getElementById('q5A1').className = 'item2clicked';
+	document.getElementById('q5A2').className = 'item3';
+	document.getElementById('q5A3').className = 'item4';
+	document.getElementById('q5A4').className = 'item5';
+	window.localStorage.setItem("q5", "1");
+}
+
+function a2Q5() {
+	document.getElementById('q5A1').className = 'item2';
+	document.getElementById('q5A2').className = 'item3clicked';
+	document.getElementById('q5A3').className = 'item4';
+	document.getElementById('q5A4').className = 'item5';
+	window.localStorage.setItem("q5", "2");
+}
+
+function a3Q5() {
+	document.getElementById('q5A1').className = 'item2';
+	document.getElementById('q5A2').className = 'item3';
+	document.getElementById('q5A3').className = 'item4clicked';
+	document.getElementById('q5A4').className = 'item5';
+	window.localStorage.setItem("q5", "3");
+}
+
+function a4Q5() {
+	document.getElementById('q5A1').className = 'item2';
+	document.getElementById('q5A2').className = 'item3';
+	document.getElementById('q5A3').className = 'item4';
+	document.getElementById('q5A4').className = 'item5clicked';
+	window.localStorage.setItem("q5", "4");
+}
+
+function a1Q6() {
+	document.getElementById('q6A1').className = 'item2clicked';
+	document.getElementById('q6A2').className = 'item3';
+	document.getElementById('q6A3').className = 'item4';
+	document.getElementById('q6A4').className = 'item5';
+	window.localStorage.setItem("q6", "1");
+}
+
+function a2Q6() {
+	document.getElementById('q6A1').className = 'item2';
+	document.getElementById('q6A2').className = 'item3clicked';
+	document.getElementById('q6A3').className = 'item4';
+	document.getElementById('q6A4').className = 'item5';
+	window.localStorage.setItem("q6", "2");
+}
+
+function a3Q6() {
+	document.getElementById('q6A1').className = 'item2';
+	document.getElementById('q6A2').className = 'item3';
+	document.getElementById('q6A3').className = 'item4clicked';
+	document.getElementById('q6A4').className = 'item5';
+	window.localStorage.setItem("q6", "3");
+}
+
+function a4Q6() {
+	document.getElementById('q6A1').className = 'item2';
+	document.getElementById('q6A2').className = 'item3';
+	document.getElementById('q6A3').className = 'item4';
+	document.getElementById('q6A4').className = 'item5clicked';
+	window.localStorage.setItem("q6", "4");
+}
+
+function a1Q7() {
+	document.getElementById('q7A1').className = 'item2clicked';
+	document.getElementById('q7A2').className = 'item3';
+	document.getElementById('q7A3').className = 'item4';
+	document.getElementById('q7A4').className = 'item5';
+	window.localStorage.setItem("q7", "1");
+}
+
+function a2Q7() {
+	document.getElementById('q7A1').className = 'item2';
+	document.getElementById('q7A2').className = 'item3clicked';
+	document.getElementById('q7A3').className = 'item4';
+	document.getElementById('q7A4').className = 'item5';
+	window.localStorage.setItem("q7", "2");
+}
+
+function a3Q7() {
+	document.getElementById('q7A1').className = 'item2';
+	document.getElementById('q7A2').className = 'item3';
+	document.getElementById('q7A3').className = 'item4clicked';
+	document.getElementById('q7A4').className = 'item5';
+	window.localStorage.setItem("q7", "3");
+}
+
+function a4Q7() {
+	document.getElementById('q7A1').className = 'item2';
+	document.getElementById('q7A2').className = 'item3';
+	document.getElementById('q7A3').className = 'item4';
+	document.getElementById('q7A4').className = 'item5clicked';
+	window.localStorage.setItem("q7", "4");
+}
+
+function a1Q8() {
+	document.getElementById('q8A1').className = 'item2clicked';
+	document.getElementById('q8A2').className = 'item3';
+	document.getElementById('q8A3').className = 'item4';
+	document.getElementById('q8A4').className = 'item5';
+	window.localStorage.setItem("q8", "1");
+}
+
+function a2Q8() {
+	document.getElementById('q8A1').className = 'item2';
+	document.getElementById('q8A2').className = 'item3clicked';
+	document.getElementById('q8A3').className = 'item4';
+	document.getElementById('q8A4').className = 'item5';
+	window.localStorage.setItem("q8", "2");
+}
+
+function a3Q8() {
+	document.getElementById('q8A1').className = 'item2';
+	document.getElementById('q8A2').className = 'item3';
+	document.getElementById('q8A3').className = 'item4clicked';
+	document.getElementById('q8A4').className = 'item5';
+	window.localStorage.setItem("q8", "3");
+}
+
+function a4Q8() {
+	document.getElementById('q8A1').className = 'item2';
+	document.getElementById('q8A2').className = 'item3';
+	document.getElementById('q8A3').className = 'item4';
+	document.getElementById('q8A4').className = 'item5clicked';
+	window.localStorage.setItem("q8", "4");
+}
+
+function results() {
+	let result1 =  localStorage.getItem("q1");
+	let result2 =  localStorage.getItem("q2");
+	let result3 =  localStorage.getItem("q3");
+	let result4 =  localStorage.getItem("q4");
+	let result5 =  localStorage.getItem("q5");
+	let result6 =  localStorage.getItem("q6");
+	let result7 =  localStorage.getItem("q7");
+	let result8 =  localStorage.getItem("q8");
+	
+	let mark1;
+	let mark2;
+	let mark3;
+	let mark4;
+	let mark5;
+	let mark6;
+	let mark7;
+	let mark8;
+	
+	let counter = 0
+	
+	if (result1 == "3") {
+		mark1 = "Right";
+		counter++
+	} else {
+		mark1 = "Wrong";
+	}
+	
+	if (result2 == "1") {
+		mark2 = "Right";
+		counter++
+	} else {
+		mark2 = "Wrong";
+	}
+	
+	if (result3 == "4") {
+		mark3 = "Right";
+		counter++
+	} else {
+		mark3 = "Wrong";
+	}
+	
+	if (result4 == "1") {
+		mark4 = "Right";
+		counter++
+	} else {
+		mark4 = "Wrong";
+	}
+	
+	if (result5 == "2") {
+		mark5 = "Right";
+		counter++
+	} else {
+		mark5 = "Wrong";
+	}
+	
+	if (result6 == "4") {
+		mark6 = "Right";
+		counter++
+	} else {
+		mark6 = "Wrong";
+	}
+	
+	if (result7 == "1") {
+		mark7 = "Right";
+		counter++
+	} else {
+		mark7 = "Wrong";
+	}
+	
+	if (result8 == "4") {
+		mark8 = "Right";
+		counter++
+	} else {
+		mark8 = "Wrong";
+	}
+	
+	document.getElementById("displayMark1").innerHTML = mark1;
+	document.getElementById("displayMark2").innerHTML = mark2;
+	document.getElementById("displayMark3").innerHTML = mark3;
+	document.getElementById("displayMark4").innerHTML = mark4;
+	document.getElementById("displayMark5").innerHTML = mark5;
+	document.getElementById("displayMark6").innerHTML = mark6;
+	document.getElementById("displayMark7").innerHTML = mark7;
+	document.getElementById("displayMark8").innerHTML = mark8;
+	document.getElementById("score").innerHTML = counter;
+}
+
+function resetScores() {
+	window.localStorage.setItem("q1", "0");
+	window.localStorage.setItem("q2", "0");
+	window.localStorage.setItem("q3", "0");
+	window.localStorage.setItem("q4", "0");
+	window.localStorage.setItem("q5", "0");
+	window.localStorage.setItem("q6", "0");
+	window.localStorage.setItem("q7", "0");
+	window.localStorage.setItem("q8", "0");
+}
+
